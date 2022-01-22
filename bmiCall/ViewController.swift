2b6bc3227ca7b99ec8a746b32cebf80780a98f59
Kookjungkeeen <cat3668@naver.com>
2022-01-23 00:31:59 +0900

    Initial Commit

---
+++ bmiCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmiCall
+//
+//  Created by 국정근 on 2022/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
